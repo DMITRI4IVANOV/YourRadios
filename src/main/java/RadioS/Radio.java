11aa4e6394352a859ce0e5def8c3d6f7ffe16622
DMITRI4IVANOV <DmitryIvanovAN@yandex.ru>
2022-05-14 20:48:38 +0400

    Not failed

--- src/main/java/RadioS/Radio.java
+++ src/main/java/RadioS/Radio.java
@@ -34,12 +34,12 @@ public class Radio {
     }
 
     public void setCurrentStation(int currentStation) {
-        if (currentStation < 0) {
+        if (currentStation <= 0) {
             return;
         }
-        if (currentStation > 9) {
-            return;
-        }
+//        if (currentStation > 9) {
+//            return;
+//        }
         this.currentStation = currentStation;
     }
 
@@ -48,12 +48,12 @@ public class Radio {
     }
 
     public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) {
+        if (currentVolume <= 0) {
             return;
         }
-        if (currentVolume > 10) {
-            return;
-        }
+//        if (currentVolume > 10) {
+//            return;
+//        }
         this.currentVolume = currentVolume;
     }
 }
