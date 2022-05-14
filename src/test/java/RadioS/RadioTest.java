cfb185e6bf1b672301d9bfd661e89d4f24fe8b9d
DMITRI4IVANOV <DmitryIvanovAN@yandex.ru>
2022-05-14 20:06:12 +0400

    first commit

---
+++ src/test/java/RadioS/RadioTest.java
@@ -0,0 +1,216 @@
+package RadioS;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+    @Test
+//    Около граничного значения ( для кнопки Next)
+    void sholdFrontoftheBorder1NextForStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(8);
+        radio.next();
+
+        int actual = radio.getCurrentStation();
+        int expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Около граничного значения ( для кнопки Next)
+    void sholdFrontoftheBorder2NextForStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(1);
+        radio.next();
+
+        int actual = radio.getCurrentStation();
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Граничное значение ( для кнопки Next)
+    void sholdBoundary1NextForStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.next();
+
+        int actual = radio.getCurrentStation();
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Граничное значение ( для кнопки Next)
+    void sholdBoundary2NextForStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        radio.next();
+
+        int actual = radio.getCurrentStation();
+        int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Граничное значение ( для кнопки Prev)
+    void sholdBoundary1PrevForStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        radio.prev();
+
+        int actual = radio.getCurrentStation();
+        int expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Граничное значение ( для кнопки Prev)
+    void sholdBoundary2PrevForStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.prev();
+
+        int actual = radio.getCurrentStation();
+        int expected = 8;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Около граничного значения ( для кнопки Prev)
+    void sholdFrontoftheBorder1PrevForStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(8);
+        radio.prev();
+
+        int actual = radio.getCurrentStation();
+        int expected = 7;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Около граничного значения ( для кнопки Prev)
+    void sholdFrontoftheBorder2PrevForStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(1);
+        radio.prev();
+
+        int actual = radio.getCurrentStation();
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Граничное значение для звука ( кнопка increaseVolume "+")
+    void sholdBoundary1IncreaseVolumeForVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        radio.increaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Граничное значение для звука ( кнопка increaseVolume "+")
+    void sholdBoundary2IncreaseVolumeForVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(10);
+        radio.increaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 10;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Граничное значение для звука ( кнопка decreaseVolume "-")
+    void sholdBoundary1DecreaseVolumeForVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(10);
+        radio.decreaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Граничное значение для звука ( кнопка decreaseVolume "-")
+    void sholdBoundary2DecreaseVolumeForVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        radio.decreaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Около граничного значения для звука ( кнопка increaseVolume "+")
+    void sholdFrontoftheBorder1IncreaseVolumeForVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(9);
+        radio.increaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 10;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Около граничного значения для звука ( кнопка increaseVolume "+")
+    void sholdFrontoftheBorder2IncreaseVolumeForVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        radio.increaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Около граничного значения для звука ( кнопка decreaseVolume "-")
+    void sholdFrontoftheBorder1DecreaseVolumeForVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(9);
+        radio.decreaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 8;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+//    Около граничного значения для звука ( кнопка decreaseVolume "-")
+    void sholdFrontoftheBorder2DecreaseVolumeForVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(1);
+        radio.decreaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
